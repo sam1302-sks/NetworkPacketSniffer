b787983c221313752a691e6c4af1c032414349e0
samarth-sachin <samarthpatil@yahoo.com>
2025-09-07 12:40:49 +0530

    basic

---
+++ src/main/java/com/example/NetworkPacket/NetworkPacketApplication.java
@@ -0,0 +1,13 @@
+package com.example.NetworkPacket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkPacketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkPacketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NetworkPacket/NetworkPacketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NetworkPacket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkPacketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
